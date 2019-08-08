f0b66ea97b199abc1ce3512a5169febcbcb4b587
Chris Johnson <chris@rebzmedia.com>
2019-08-08 18:41:41 -0400

    Let's update the example... not in the README

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
-import Scraper from './src/classes/Scraper'
+import Scraper from './src/index'
 
-// this is an example, it does not actually work
 const config = {
-    uri: 'http://www.example.com',
+    uri: 'https://www.example.com/',
+    pagination: {
+        selector: 'div.prev_next',
+        handler: async (currentUri: string, uris: any) => {
+            const url = new URL(currentUri)
+            const newPath = uris[Object.keys(uris).filter(k => k.indexOf('Next') != -1)[0]]
+            return newPath && url.origin+newPath
+        }
+    },
     selectors: {
         id: 'el#id',
         class: 'el.class',
         attr: 'el[attr="val"]',
+        specifictychainsaresafehere: 'ul.news > li.first > p.title span[data-why-would-this-exist="..."]',
     }
 };
-const site = new Scraper(config);
-site.scrape().then(console.log)
+
+(async () => {
+    const scraper = Scraper().setConfig(config);
+    scraper.errors(console.error)
+    scraper.listen(console.log)
+    await scraper.autoScrape() // or scrape a single page: `scraper.scrape()`
+    if (scraper.stop()) {
+        console.info('done scraping')
+    }
+})()
+
+/*  Parse HTML using DOMParser
+    var xmlString = "<div id='foo'><a href='#'>Link</a><span></span></div>";
+    doc = new DOMParser().parseFromString(xmlString, "text/xml");
+    console.log(doc.firstChild.innerHTML); // => <div id="foo">...
+    console.log(doc.firstChild.firstChild.innerHTML); // => <a href="#">..
+*/
